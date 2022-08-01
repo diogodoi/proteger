68ea9ec202144ad583d71e1cfb363f79d2e76d23
Diogo Godoi <42503205+diogodoi@users.noreply.github.com>
2022-08-01 14:20:36 -0300

    Comando imagem sessao adicionado
    Foi adicionado um comando que cria a pasta imagemSessao para salvar as imagens do detector de imagem

--- Start.py
+++ Start.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from PyQt4 import QtGui, QtCore
+import os
+import sqlite3
 
 from interface import Ui_MainWindow
 import sys
@@ -13,6 +15,38 @@ class interface(QtGui.QMainWindow,Ui_MainWindow):
         
 
 if __name__ == "__main__":
+    #Verifica se o banco de dados existe, se não existir ele é criado.
+    try:
+        bd = open('BdProteger.db')
+        bd.close()
+    except:
+        conn = sqlite3.connect('BdProteger.db')
+        conn.execute(''' CREATE TABLE "IpRobot" (
+        "id"	INTEGER,
+        "IpRobot"	TEXT UNIQUE,
+        PRIMARY KEY("id"))
+                    ''')
+        conn.execute(''' 
+        CREATE TABLE "Sessao" (
+        "ip"	INTEGER UNIQUE,
+        "Data"	TEXT NOT NULL,
+        "Hora"	TEXT NOT NULL,
+        "Aviso"	TEXT NOT NULL,
+        PRIMARY KEY("ip" AUTOINCREMENT))    
+    ''')
+        conn.execute("INSERT INTO IpRobot (IpRobot) VALUES ('192.168.0.1')");
+        conn.commit()
+        conn.close()
+        
+    try:
+        os.mkdir("Logs")       
+    except:
+        pass
+    try:
+        os.mkdir("imagensSessao")       
+    except:
+        pass
+    
     app = QtGui.QApplication(sys.argv)
     window = interface()
     window.show()
