2b54fb6f9ee704635dd8ae04944737720bf3f121
Diogo Godoi <42503205+diogodoi@users.noreply.github.com>
2023-07-20 17:32:57 -0300

    Add files via upload

---
+++ movimentos/respiracao.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+def Respiracao():
+    names = list()
+    times = list()
+    keys = list()
+    
+    names.append("HeadPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.361982, -0.09515, 0.361981, -0.0951499, 0.361981, -0.0951499])
+    
+    names.append("HeadYaw")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.0629361, -0.0598679, -0.062936, -0.0598679, -0.062936, -0.0598679])
+    
+    names.append("LAnklePitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.0919981, 0.0858622, 0.091998, 0.0858622, 0.091998, 0.0858622])
+    
+    names.append("LAnkleRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.125746, -0.125746, -0.125746, -0.125746, -0.125746, -0.125746])
+    
+    names.append("LElbowRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.961776, -0.776162, -0.961776, -0.776162, -0.961776, -0.776162])
+    
+    names.append("LElbowYaw")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.145772, -1.0631, -0.145772, -1.0631, -0.145772, -1.0631])
+    
+    names.append("LHand")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.6276, 0.6276, 0.6276, 0.6276, 0.6276, 0.6276])
+    
+    names.append("LHipPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.348176, 0.00157595, -0.348176, 0.00157595, -0.348176, 0.00157595])
+    
+    names.append("LHipRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.10282, 0.104354, 0.10282, 0.104354, 0.10282, 0.104354])
+    
+    names.append("LHipYawPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.18097, -0.182504, -0.18097, -0.182504, -0.18097, -0.182504])
+    
+    names.append("LKneePitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.283748, -4.19617e-05, 0.283749, -4.19617e-05, 0.283749, -4.19617e-05])
+    
+    names.append("LShoulderPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([1.00626, 1.17193, 1.00626, 1.17193, 1.00626, 1.17193])
+    
+    names.append("LShoulderRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.082794, -0.135034, 0.0827939, -0.135034, 0.0827939, -0.135034])
+    
+    names.append("LWristYaw")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.77778, -0.283832, -0.77778, -0.283832, -0.77778, -0.283832])
+    
+    names.append("RAnklePitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.090548, 0.0890141, 0.090548, 0.0890141, 0.090548, 0.0890141])
+    
+    names.append("RAnkleRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.127364, 0.127364, 0.127364, 0.127364, 0.127364, 0.127364])
+    
+    names.append("RElbowRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([1.24258, 1.25332, 1.24258, 1.25332, 1.24258, 1.25332])
+    
+    names.append("RElbowYaw")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.029104, 0.029104, 0.0291041, 0.0291041, 0.0291041, 0.0291041])
+    
+    names.append("RHand")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.638, 0.638, 0.638, 0.638, 0.638, 0.638])
+    
+    names.append("RHipPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.349794, -4.19617e-05, -0.349794, -4.19617e-05, -0.349794, -4.19617e-05])
+    
+    names.append("RHipRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.10427, -0.101202, -0.10427, -0.101202, -0.10427, -0.101202])
+    
+    names.append("RHipYawPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.18097, -0.182504, -0.18097, -0.182504, -0.18097, -0.182504])
+    
+    names.append("RKneePitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.2869, 0.00464392, 0.286901, 0.00464392, 0.286901, 0.00464392])
+    
+    names.append("RShoulderPitch")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([0.392746, 0.392746, 0.392746, 0.392746, 0.392746, 0.392746])
+    
+    names.append("RShoulderRoll")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([-0.021518, -0.021518, -0.021518, -0.021518, -0.021518, -0.021518])
+    
+    names.append("RWristYaw")
+    times.append([1.6, 4, 7.2, 9.6, 12.8, 15.2])
+    keys.append([1.2517, 1.2517, 1.2517, 1.2517, 1.2517, 1.2517])
+    
+    
+    return names, times, keys, "Respiração"
+    
+    
