9a17b308e308a06450f554e4d044b482b780a3fd
diogo-godoi-quattour <dhg23496@gmail.com>
2025-03-13 18:42:41 -0300

    Update

--- exec.bat
+++ exec.bat
@@ -2,5 +2,6 @@ CreateObject("Wscript.Shell").Run "your_batch_file.bat", 0, True
 ECHO ON
 REM A batch script to execute a Python script
 SET PATH=%PATH%;C:\Python27
+pip install opencv-contrib-python==4.1.0.25
 python ./Start.py
 PAUSE
--- retrivingImages.py
+++ retrivingImages.py
@@ -14,9 +14,6 @@ import vision_definitions
 import cv2
 import numpy as np
 
-from tensorflow.python.keras.models import load_model
-# Para salvar o modelo no formato json
-from tensorflow.python.keras.models import model_from_json
 
 class NAOimageRetriving(QWidget):
     """
@@ -49,24 +46,6 @@ class NAOimageRetriving(QWidget):
 
         # Trigget 'timerEvent' every 100 ms.
         self.startTimer(100)
-
-        # self.tempoI = time.strftime("%H:%M:%S", time.localtime())
-        # self.tempoI = datetime.now()
-        
-        # arquivo_modelo = 'cnn_expressoes1.h5' # referente aos pesos
-        # arquivo_modelo_json = 'cnn_expressoes1.json' # referente a arquitetura da Rede Neural
-        
-        # # Código para recepção do open (carregando o modelo salvo no item 6)
-        # json_file = open(arquivo_modelo_json, 'r')
-        # loaded_model_json = json_file.read()
-        # json_file.close() # liberação de memória 
-
-        # # Fazendo a leitura do arquivo json para transformar esse para o modelo Tensorflow
-        # self.expressoes = ['Medo','Feliz','Triste','Neutro']
-        # self.loaded_model = model_from_json(loaded_model_json)
-        # self.loaded_model.load_weights(arquivo_modelo)
-
-        # self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
         
         self.noSignal = cv2.imread('./imagens/NOSIGNAL.png')
         
@@ -111,10 +90,7 @@ class NAOimageRetriving(QWidget):
             imageHeight = self._alImage[1] # Height.
             array = self._alImage[6] # Pixel array.        
             image = np.fromstring(array, np.uint8).reshape(imageHeight, imageWidth, 3 )
-            opencvImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)            
-            # emotion = self.findFace(opencvImage)
-            # if emotion!=None:
-            #     cv2.putText(opencvImage,emotion[0]+" "+emotion[1] , (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)                         
+            opencvImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)                                    
              
         except: opencvImage = self.noSignal
         self._image =  QImage(opencvImage,           # Pixel array.
@@ -130,25 +106,6 @@ class NAOimageRetriving(QWidget):
         """
         self._updateImage()
         self.update()
-
-    # def findFace(self,face):
-    #     faces = self.face_cascade.detectMultiScale(face, 1.04, 5)
-    #     lista_frames = []
-    #     if len(faces) == 0:
-    #         return None
-    #     else:
-    #         for x,y,w,h in faces:
-    #             face_cut = face[y:y+h,x:x+w]
-    #             resized = cv2.resize(face_cut, (160, 120))
-    #             roi_gray = resized.astype('float')/255
-    #             lista_frames.append(roi_gray)
-    #             try:                        
-    #                 prediction = self.loaded_model.predict(np.array(lista_frames))
-    #                 if np.max(prediction[-1]) < 0.6:
-    #                     return None
-    #                 emotion = [self.expressoes[int(np.argmax(prediction[-1]))],str(np.max(prediction[-1]))[0:4]]
-    #                 return emotion
-    #             except:return None
     
                      
 
